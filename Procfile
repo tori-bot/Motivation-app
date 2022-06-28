f5a07f19b2b5deea5bd35f1ae5b16bdc6abfb842
victoria makena <makenavictoria1@gmail.com>
2022-06-28 23:12:34 +0300

    deployment: create and deploy project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn motivation_project.wsgi
---
+++ motivation_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MotivationAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'motivation_app'
