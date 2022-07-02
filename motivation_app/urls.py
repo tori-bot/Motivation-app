f0561f1f060805b5d0c4e60e41a0b0c653f19223
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-07-02 16:49:46 +0300

    ts:category-addition - tests POST method for a category

--- motivation_app/urls.py
+++ motivation_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home,name='home'),
     path('signup/staff/', StaffSignUpView.as_view()),
     path('signup/student/', StudentSignUpView.as_view()),
+    path('staff/create_categories/',views.categoryCreation, name="category"),
 ]
