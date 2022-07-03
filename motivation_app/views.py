87c2bc063ca7f2237fba74c3f01bdc8b73c832da
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-07-03 11:37:27 +0300

    ft:single-post -adds methods to get a single item and update a single item

--- motivation_app/views.py
+++ motivation_app/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .api.serializers import *
 from rest_framework.decorators import api_view
@@ -26,10 +27,9 @@ def categoryCreation(request):
 
 class PostList(APIView):
     def get(self, request, format=None):
-        #querying from the database(students table)
+        #querying from the database(Posts table)
         posts = Post.objects.all()
         serializers = PostSerializer(posts, many=True)
-        
         #JSON RESPONSE
         return Response(serializers.data)
     
@@ -42,3 +42,28 @@ class PostList(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class SinglePostList(APIView):
+    # permission_classes = (IsAdminOrReadOnly,)
+    def get_single_post(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        single_post = self.get_single_post(pk)
+        serializers = PostSerializer(single_post)
+        return Response(serializers.data)
+    
+    
+    def put(self, request, pk, format=None):
+        single_post = self.get_single_post(pk)
+        serializers = PostSerializer(single_post, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
