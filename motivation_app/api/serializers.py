c4ef0eb81fb61e8cf0122357c8f4254621fd61db
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-07-12 18:27:34 +0300

    ft:user-authentication - fix user-authentication

--- motivation_app/api/serializers.py
+++ motivation_app/api/serializers.py
@@ -1,96 +1,118 @@
 from rest_framework import serializers
 from motivation_app.models import *
+from django.contrib.auth import authenticate
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['username', 'email', 'password']
         
-class AdminSignUpSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+# class AdminSignUpSerializer(serializers.ModelSerializer):
+#     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     
-    class Meta:
-        model=User
-        fields=['username', 'email','password', 'password2']
+#     class Meta:
+#         model=User
+#         fields=['username', 'email','password', 'password2']
         
-        extra_kwargs={
-            'password':{'write_only':'True'}
-        }
+#         extra_kwargs={
+#             'password':{'write_only':'True'}
+#         }
         
         
-    def save(self, **kwargs):
-        user=User(
-            username=self.validated_data['username'],
-            email=self.validated_data['email'],   
-        )
-        password=self.validated_data['password']
-        password2=self.validated_data['password']
+#     def save(self, **kwargs):
+#         user=User(
+#             username=self.validated_data['username'],
+#             email=self.validated_data['email'],   
+#         )
+#         password=self.validated_data['password']
+#         password2=self.validated_data['password']
         
-        if password != password2:
-            raise serializers.ValidationError({'error':'check your passwords'})
-        user.set_password(password)
-        user.is_admin=True
-        user.save()
-        Admin.objects.create(user=user)
-        return user
+#         if password != password2:
+#             raise serializers.ValidationError({'error':'check your passwords'})
+#         user.set_password(password)
+#         user.is_admin=True
+#         user.save()
+#         Admin.objects.create(user=user)
+#         return user
         
 
-class StaffSignUpSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+# class StaffSignUpSerializer(serializers.ModelSerializer):
+#     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     
-    class Meta:
-        model=User
-        fields=['username', 'email','password', 'password2']
+#     class Meta:
+#         model=User
+#         fields=['username', 'email','password', 'password2']
         
-        extra_kwargs={
-            'password':{'write_only':'True'}
-        }
+#         extra_kwargs={
+#             'password':{'write_only':'True'}
+#         }
         
         
-    def save(self, **kwargs):
-        user=User(
-            username=self.validated_data['username'],
-            email=self.validated_data['email'],   
-        )
-        password=self.validated_data['password']
-        password2=self.validated_data['password']
+#     def save(self, **kwargs):
+#         user=User(
+#             username=self.validated_data['username'],
+#             email=self.validated_data['email'],   
+#         )
+#         password=self.validated_data['password']
+#         password2=self.validated_data['password']
         
-        if password != password2:
-            raise serializers.ValidationError({'error':'check your passwords'})
-        user.set_password(password)
-        user.is_staff=True
-        user.save()
-        Staff.objects.create(user=user)
-        return user
+#         if password != password2:
+#             raise serializers.ValidationError({'error':'check your passwords'})
+#         user.set_password(password)
+#         user.is_staff=True
+#         user.save()
+#         Staff.objects.create(user=user)
+#         return user
             
-class StudentSignUpSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+# class StudentSignUpSerializer(serializers.ModelSerializer):
+#     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     
-    class Meta:
-        model=User
-        fields=['username', 'email','password', 'password2']
+#     class Meta:
+#         model=User
+#         fields=['username', 'email','password', 'password2']
         
-        extra_kwargs={
-            'password':{'write_only':'True'}
-        }
+#         extra_kwargs={
+#             'password':{'write_only':'True'}
+#         }
         
         
-    def save(self, **kwargs):
-        user=User(
-            username=self.validated_data['username'],
-            email=self.validated_data['email'],   
-        )
-        password=self.validated_data['password']
-        password2=self.validated_data['password']
+#     def save(self, **kwargs):
+#         user=User(
+#             username=self.validated_data['username'],
+#             email=self.validated_data['email'],   
+#         )
+#         password=self.validated_data['password']
+#         password2=self.validated_data['password']
         
-        if password != password2:
-            raise serializers.ValidationError({'error':'check your passwords'})
-        user.set_password(password)
-        user.is_student=True
-        user.save()
-        Student.objects.create(user=user)
-        return user
+#         if password != password2:
+#             raise serializers.ValidationError({'error':'check your passwords'})
+#         user.set_password(password)
+#         user.is_student=True
+#         user.save()
+#         Student.objects.create(user=user)
+#         return user
             
+            
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = "__all__"
+        read_only_fields = ["name"]
+        
+        
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def validate_user(self):
+        user = authenticate(
+            username=self.validated_data["username"],
+            password=self.validated_data["password"],
+        )
+        if user is None:
+            raise serializers.ValidationError("Invalid Credentials")
+        return user
         
 #User profile serializers
 class ProfileSerializer(serializers.ModelSerializer):
@@ -99,6 +121,19 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ("pk", 'user','firstname','lastname','email','profile_pic','bio') 
         
         
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name","password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+   
+        
+        
+
 class CategorySerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     class Meta:
@@ -112,12 +147,21 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model=Post
         fields = '__all__'
-        
+       
+     
 class CommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     class Meta:
         model=Comment
         fields = '__all__'
+        # fields = (
+        #     'id',
+        #      user''
+        #     'comment',
+        #     'date_posted',
+        #     'user_id',
+        #     'post_id'
+        # )
         
 class ChildCommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
@@ -144,3 +188,9 @@ class UserProfileChangeSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        
+class GetUserSerializer(serializers.ModelSerializer):
+    # role = RoleSerializer(read_only=True)
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name", "role"]
